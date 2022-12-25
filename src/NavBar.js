a50f7e2934d8c72d7c071abdcf0121a17c4c86b8
Daniel <daniel@daniel-HP-Pavilion-Sleekbook-15-PC.(none)>
2022-12-25 16:32:06 +0100

    adding footer component and go back links

--- src/NavBar.js
+++ src/NavBar.js
@@ -27,13 +27,14 @@ class NavBar extends Component {
     this.setState({ open: false });
   }
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showSlider } = this.props;
     const { format } = this.state;
     return (
       <header className='Navbar'>
         <div className='logo'>
           <Link to='/'>reactcolorpicker</Link>
         </div>
+        {showSlider && (
         <div className='slider-container'>
           <span>Level: {level}</span>
           <div className='slider'>
@@ -46,6 +47,7 @@ class NavBar extends Component {
             />
           </div>
         </div>
+        )}
         <div className='select-container'>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value='hex'>HEX - #ffffff</MenuItem>
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'; 
 import ColorBox from './ColorBox';
+import NavBar from './NavBar';
+import PaletteFooter from './PaletteFooter';
+import { Link } from 'react-router-dom';
 
 class SingleColorPalette extends Component{
     constructor(props){
         super(props);
-        this._shades = this.gatherColors(this.props.palette, this.props.colorId)
-        console.log(this._shades)
+        this._shades = this.gatherColors(this.props.palette, this.props.colorId);
+        this.state = {format: "hex"}
+        this.changeFormat = this.changeFormat.bind(this);
     }
     gatherColors(palette, colorToFilterBy){
         let shades = [];
@@ -17,16 +21,27 @@ class SingleColorPalette extends Component{
         }
         return shades.slice(1);
     }
+    changeFormat(val) {
+        this.setState({ format: val });
+      }
     render(){
+        const {paletteName, emoji, id} = this.props.palette
         const colorBoxes = this._shades.map(color => 
-            <ColorBox key={color.id} name={color.name} background={color.hex} showLink={false}/>
+            <ColorBox key={color.name} name={color.name} background={color[this.state.format]}  showLink={false}/>
             )
         return(
-            <div className='Palette'>
-                <h1>Single Color Palette</h1>
+            <div className='SingleColorPalette Palette'>
+                <NavBar
+                handleChange={this.changeFormat}
+                showSlider={false}
+                />
                 <div className='Palette-colors'>
                     {colorBoxes}
+                    <div className='ColorBox go-back'>
+                        <Link to={`/palette/${id}`} className='back-button'>Go back !</Link>
+                    </div>
                 </div>
+                <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         )
     }
