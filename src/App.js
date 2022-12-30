8e95346d205863a5364e12443ae0b447c7605793
Daniel <daniel@daniel-HP-Pavilion-Sleekbook-15-PC.(none)>
2022-12-30 17:09:20 +0100

    saving palettes to local storage

--- src/App.js
+++ src/App.js
@@ -10,19 +10,23 @@ import SingleColorPalette from "./SingleColorPalette";
 class App extends Component {
   constructor(props){
     super(props); 
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'))
     this.state = {
-      palettes: seedColors
+      palettes: savedPalettes || seedColors,
     }
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
   }
   savePalette(newPalette){
-    this.setState({palettes: [...this.state.palettes, newPalette]})
+    this.setState({palettes: [...this.state.palettes, newPalette]}, this.syncLocalStorage)
   }
   findPalette(id) {
     return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
+  }
+  syncLocalStorage(){
+    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes))
   }
   render() {
     return (
